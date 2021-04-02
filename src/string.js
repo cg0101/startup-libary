d3a180eb76ab7a950f94c8323791581d989085ad
zhangchi05 <zhangchi05@kuaishou.com>
2021-04-02 11:28:45 +0800

    feat: auto commit

--- src/string.js
+++ src/string.js
@@ -6,5 +6,11 @@ export default {
     // 首字母转大写
     capitalize: function (string) {
         return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+    },
+    insertStr(source, start, newStr) {
+        return source.slice(0, start) + newStr + source.slice(start);
+    },
+    removeStr(source, index) {
+        return source.slice(0, index) + source.slice(index + 1);
     }
 };
